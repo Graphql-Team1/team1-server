16894bd0a912a55b8fdce2172aee20dee9d52a3e
sung-silver <jinna0319@gmail.com>
2024-06-17 00:29:17 +0900

    [INIT] project 초기셋팅

---
+++ graphQL/src/main/kotlin/com/sopt/team1/GraphQlApplication.kt
@@ -0,0 +1,11 @@
+package com.sopt.team1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphQlApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphQlApplication>(*args)
+}
