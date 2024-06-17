2f56e5bbc8ef7c282c55c29ff398a76fecf6e276
sung-silver <jinna0319@gmail.com>
2024-06-17 22:14:04 +0900

    [FEAT] EntityNotFoundException 에러 처리 구현

---
+++ graphQL/src/main/kotlin/com/sopt/team1/common/CustomExceptionResolver.kt
@@ -0,0 +1,18 @@
+package com.sopt.team1.common
+
+import graphql.GraphQLError
+import graphql.schema.DataFetchingEnvironment
+import jakarta.persistence.EntityNotFoundException
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter
+import org.springframework.graphql.execution.ErrorType
+import org.springframework.stereotype.Component
+
+@Component
+class CustomExceptionResolver : DataFetcherExceptionResolverAdapter() {
+    override fun resolveToSingleError(ex: Throwable, env: DataFetchingEnvironment): GraphQLError? {
+        if(ex is EntityNotFoundException){
+            return GraphQLError.newError().errorType(ErrorType.NOT_FOUND).message(ex.message).build()
+        }
+        return super.resolveToSingleError(ex, env)
+    }
+}
