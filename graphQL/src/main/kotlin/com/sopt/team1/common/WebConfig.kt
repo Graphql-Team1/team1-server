5bafb34d08d7cc75839ace705ce8f3aebe6ee862
sung-silver <jinna0319@gmail.com>
2024-06-17 22:27:16 +0900

    [FEAT] CORS 처리

---
+++ graphQL/src/main/kotlin/com/sopt/team1/common/WebConfig.kt
@@ -0,0 +1,25 @@
+package com.sopt.team1.common
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    @Value("\${allowed.allowed-origins}")
+    lateinit var allowedOrigins: String
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins(allowedOrigins)
+                    .allowedMethods("POST")
+            }
+        }
+    }
+}
